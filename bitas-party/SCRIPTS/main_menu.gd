d683d8f8af7bb8768a84c7f58c8f8d2d94e2ff57
mnedel-ops <m.nedel@ufn.edu.br>
2026-05-21 19:02:03 -0300

    desfeito as coisas da ui
    vou soh fazer as imagens

--- bitas-party/SCRIPTS/main_menu.gd
+++ bitas-party/SCRIPTS/main_menu.gd
@@ -1,13 +1,4 @@
 extends Control
 
-@export var player_config_scene: PackedScene
-
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		go_to_player_config()
-
-func go_to_player_config() -> void:
-	if player_config_scene:
-		get_tree().change_scene_to_packed(player_config_scene)
-	else:
-		print("Erro: player_config_scene não foi configurada!")
+	pass
